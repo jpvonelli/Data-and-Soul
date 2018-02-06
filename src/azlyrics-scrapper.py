072ed04747260037b04d778348ae043c8ab561f4
John Vonelli <stjpv03@moravian.edu>
2018-02-06 13:11:16 -0500

    Added azlyrics file

--- src/azlyrics-scrapper.py
+++ src/azlyrics-scrapper.py
@@ -1,6 +1,7 @@
 import re
 import urllib.request
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 def get_lyrics(artist, song_title):
     artist = artist.lower()
@@ -20,12 +21,48 @@ def get_lyrics(artist, song_title):
         down_partition = '<!-- MxM banner -->'
         lyrics = lyrics.split(up_partition)[1]
         lyrics = lyrics.split(down_partition)[0]
-        lyrics = lyrics.replace('<br>', '').replace('</br>', '').replace('</div>', '').strip()
+        lyrics = lyrics.replace('<br>', '').replace('</br>', '').replace('</div>', '').replace('<br/>', '').strip()
         return lyrics
     except Exception as e:
         return "Exception occurred \n" + str(e)
 
+lyrics_outfile = open("missing_lyrics.txt", "w")
 
-lyrics = get_lyrics("the beatles", "Eight Days a Week")
-print(lyrics)
+client = MongoClient()
+# DOUBLE CHECK!!!!
+# FOR CORRECT DB!
+db = client.spotify_ingest
+songs = db.songs
+billboard_ranks = db.billboard_ranks
 
+for song in songs.find():
+    song_artist = song['artist']['name']
+    song_name = song['name']
+    song_id = song['_id']
+
+    print(song_name, song_artist, song_id)
+
+    if "featuring" in song_artist.lower():
+        song_artist = song_artist.split("Featuring", 1)[0]
+
+    try:
+        lyrics = get_lyrics(song_artist, song_name)
+        print(type(lyrics))
+    except Exception as ex:
+        lyrics_outfile.write(song_artist + ", " + song_name + ": " + "Lyric information not found")
+        print(song_artist + ", " + song_name + ": " + "Lyric information not found")
+        continue
+
+    songs.update({
+        "_id": song_id
+        },
+        {
+            "$set":{
+            "lyrics":{
+                    "text": lyrics
+                }
+            }
+        }
+    )
+
+lyrics_outfile.close()
